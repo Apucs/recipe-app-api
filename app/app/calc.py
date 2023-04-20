a48e61c29c5ead7cb3a017b215034cbc274f1950
Apucs <apu3660@gmail.com>
2023-04-20 14:46:43 +0600

    added "psycopg2"

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator funciton
+"""
+
+def add(x, y):
+    """Add x and y and return the result"""
+
+    return x+y
+
+
+def subtract(x, y):
+    """Subtract x and y and return the result"""
+
+    return x-y
